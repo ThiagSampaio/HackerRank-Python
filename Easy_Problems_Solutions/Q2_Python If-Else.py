c000e21cfb001a299a1b9265a9d1e626b5bfac84
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-06 19:12:01 -0300

    Q2

---
+++ Easy_Problems_Solutions/Q2_Python If-Else.py
@@ -0,0 +1,29 @@
+"""
+Task
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
+
+Input Format:
+
+A single line containing a positive integer, n .
+
+Constraints:
+
+1<= n <= 100
+
+"""
+n = 4
+if n % 2 != 0:
+    print('Weird')
+else:
+    if n < 5:
+        print('Not Weird')
+        quit()
+    if n <= 20:
+        print('Weird')
+    else:
+        print('Not Weird')
