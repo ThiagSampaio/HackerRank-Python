bcd02623a13efd381b186a5f8d932e9741c27155
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-06 18:56:03 -0300

    first commit

---
+++ Easy_Problems_Solutions/Q1_Arithmetic_Operators.py
@@ -0,0 +1,27 @@
+"""
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+1. The first line contains the sum of the two numbers.
+2. The second line contains the difference of the two numbers (first - second).
+3. The third line contains the product of the two numbers.
+
+Example
+
+a=3
+b=5
+
+Print the following:
+
+8
+-2
+15
+
+"""
+
+#Solution -> 06/01/2022 Thiago Sampaio
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
